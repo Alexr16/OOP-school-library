e953c3577120f1f2bb58dd58b7f6a0146ff85bdc
Alexr16 <j.alex-1996@hotmail.com>
2022-06-13 13:22:22 -0500

    Add is_of_age private method

--- Person.rb
+++ Person.rb
@@ -2,10 +2,15 @@ class Person
     attr_accessor :name, :age
     attr_reader :id
 
-    def initialize(name = "Unknown", age, parent_permission = true)
+    def initialize(name = "Unknown", age, parent_permission: true)
         @id = Random.rand(1..1000)
         @name = name
         @age = age
+        @parent_permission = parent_permission
+    end
+
+    private def is_of_age?
+        @age >= 18 ? true : false
     end
 
 end
