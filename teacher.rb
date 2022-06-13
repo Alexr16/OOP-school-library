4983ab155ac553654f68143ecfe92614140d7902
Alexr16 <j.alex-1996@hotmail.com>
2022-06-13 16:54:43 -0500

    Aplu changes

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, especialization, name = 'Unknown', parent_permission: true)
+  def initialize(age, especialization, name = 'Unknown', parent_permissio = true)
     super(name, age, parent_permission)
     @especialization = especialization
   end
