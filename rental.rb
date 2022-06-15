fd83cabc6d98cd6c17181e050c070edf77fb78b1
Alexr16 <j.alex-1996@hotmail.com>
2022-06-15 11:43:39 -0500

    Create the belongs-to side of Rental, Person, and Book

--- rental.rb
+++ rental.rb
@@ -1,7 +1,14 @@
 class Rental
-    attr_accessor :date
-    
-    def initialize(date)
-        @date = date
-    end
+  attr_accessor :date
+  attr_reader :book, :person
+
+  def initialize(date, book, person)
+    @date = date
+
+    @book = book
+    book.rentals.push(self)
+
+    @person = person
+    person.rentals.push(self)
+  end
 end
