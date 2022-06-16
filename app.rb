6f3819db36749c48c43c587f1e03fe99d0bfae6b
Alexr16 <j.alex-1996@hotmail.com>
2022-06-16 09:00:35 -0500

    Add create person method

--- app.rb
+++ app.rb
@@ -19,5 +19,35 @@ class App
     puts(@people.map { |person| "[#{person.class}] Name: #{person.name}, ID: #{person.id},  Age: #{person.age}" })
   end
 
+  def create_person # rubocop:disable Metrics/MethodLength
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    option = gets.chomp
+    case option
+    when '1'
+      print 'age: '
+      age = gets.chomp.to_i
+      print 'name: '
+      name = gets.chomp.to_s
+      print 'Has parent permission? [y/n]: '
+      parent_permission = gets.chomp.to_s == 'y'
+      student = Student.new(age, name, parent_permission: parent_permission)
+      @people.push(student)
+      puts 'Person created successfully!'
+    when '2'
+      print 'age: '
+      age = gets.chomp.to_i
+      print 'name: '
+      name = gets.chomp.to_s
+      print 'Specialization: '
+      specialization = gets.chomp.to_s
+      teacher = Teacher.new(age, specialization, name, parent_permission: true)
+      @people.push(teacher)
+      puts 'Person created successfully!'
+    else
+      puts 'Invalid selection'
+      create_person
+    end
+  end
+
   
 end
