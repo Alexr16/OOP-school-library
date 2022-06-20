ac073ad307fa5ea2fc239e13e82928126979f055
Alexr16 <j.alex-1996@hotmail.com>
2022-06-20 15:18:11 -0500

    Create class menu aplying Single responsibility principle

---
+++ menu.rb
@@ -0,0 +1,27 @@
+require_relative 'app'
+
+class Menu
+  def initialize
+    @app = App.new
+  end
+
+  def process_selection(selection)
+    case selection
+    when '1'
+      @app.list_all_books
+    when '2'
+      @app.list_all_people
+    when '3'
+      @app.create_person
+    when '4'
+      @app.create_book
+    when '5'
+      @app.create_rental
+    when '6'
+      @app.list_all_rentals_by_id
+    when '7' then @app.exit
+    else
+      puts 'Invalid selection'
+    end
+  end
+end
