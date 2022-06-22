29045ce6819306ac87d3b50507bf369cb5911573
Alexr16 <j.alex-1996@hotmail.com>
2022-06-22 13:53:06 -0500

    Fix linter errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,24 +1,24 @@
 require_relative 'spec_helper'
 
 describe Book do
-  context "When testing the Book class" do
+  context 'When testing the Book class' do
     before :each do
-      @title = "Game of Thrones"
-      @author = "George R.R. Martin"
+      @title = 'Game of Thrones'
+      @author = 'George R.R. Martin'
       @book = Book.new(@title, @author)
     end
-    it "Checks to see if a variable is assigned to an instance of the Book class" do
-      @book. should be_an_instance_of Book
+    it 'Checks to see if a variable is assigned to an instance of the Book class' do
+      @book.should be_an_instance_of Book
     end
 
-    it "Checks to see if the Book instance is properly assigned Title of Game of Thrones" do
-      actual = "Game of Thrones"
+    it 'Checks to see if the Book instance is properly assigned Title of Game of Thrones' do
+      actual = 'Game of Thrones'
       expect(@book.title).to eq actual
-   end
+    end
 
-   it "Checks to see if the Book creates a proper hash representation of itself for later use of storing" do
-    actual = {title: "Game of Thrones", author: "George R.R. Martin"}
-    expect(@book.book_to_json). to eq actual
-   end
+    it 'Checks to see if the Book creates a proper hash representation of itself for later use of storing' do
+      actual = { title: 'Game of Thrones', author: 'George R.R. Martin' }
+      expect(@book.book_to_json).to eq actual
+    end
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,25 +1,25 @@
 require_relative 'spec_helper'
 
 describe Person do
-  context "When testing the Person class" do
+  context 'When testing the Person class' do
     before :each do
-      @name = "Jorge"
+      @name = 'Jorge'
       @age = 21
       @parent_permission = false
       @jorge = Person.new(@age, @name, parent_permission: @parent_permission)
     end
-    it "Checks to see if a variable is assigned to an instance of the Person class" do
-      @jorge. should be_an_instance_of Person
+    it 'Checks to see if a variable is assigned to an instance of the Person class' do
+      @jorge.should be_an_instance_of Person
     end
 
-    it "Checks to see if the Person instance is properly assigned the name of Jorge" do
-      actual = "Jorge"
+    it 'Checks to see if the Person instance is properly assigned the name of Jorge' do
+      actual = 'Jorge'
       expect(@jorge.name).to eq actual
-   end
+    end
 
-   it "Checks to see if the person object returns its correct name using the correct_name method" do
-    actual = "Jorge"
-    expect(@jorge.correct_name). to eq actual
-   end
+    it 'Checks to see if the person object returns its correct name using the correct_name method' do
+      actual = 'Jorge'
+      expect(@jorge.correct_name).to eq actual
+    end
   end
 end
