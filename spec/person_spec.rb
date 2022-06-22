4136b95042b55e8b514c08d58da1ed382a5fb223
Alexr16 <j.alex-1996@hotmail.com>
2022-06-22 17:17:14 -0500

    Write test for create rental and check can_use_services? methods

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -21,5 +21,36 @@ describe Person do
       actual = 'Jorge'
       expect(@jorge.correct_name).to eq actual
     end
+
+    it "The add_rental method returns a rental and adds its to the book" do
+      name = 'Aaron'
+      age = 21
+      parent_permission = false
+      aaron = Student.new(age, name, parent_permission: parent_permission)
+      title = 'Game of Thrones'
+      author = 'George R.R. Martin'
+      book = Book.new(title, author)
+      date = '2022/12/12'
+      rental = aaron.add_rental(date, book)
+      expect(aaron.rentals).to eql [rental]
+    end
+  end
+  
+  context "When checking permissions" do
+    
+    it "The can_use_services? method returns false if under age and no parent permission" do
+      person = Person.new(14, "Julius", parent_permission: false)
+      expect(person.can_use_services?).to be false
+    end
+    
+    it "The can_use_services? method returns true if over age" do
+      person = Person.new(19, "Mihndim", parent_permission: false)
+      expect(person.can_use_services?).to be true
+    end
+    
+    it "The can_use_services? method returns true if has parent permission" do
+      person = Person.new(14, "Rose", parent_permission: true)
+      expect(person.can_use_services?).to be true
+    end
   end
 end
